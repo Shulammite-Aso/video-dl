5c497e18265206a94ff8e148d1c48685041420fc
Shulammite-Aso <asoamarachi@gmail.com>
2021-02-24 16:40:39 +0100

    initail commit

---
+++ video-dl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# A script to install youtube-dl and download online videos on major operating systems
+# Amarachi S. Aso 22/2/2021
+
+# Check if youtube-dl is already installed
+version=$( youtube-dl --version )
+
